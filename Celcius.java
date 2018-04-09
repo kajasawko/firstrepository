cee534808c2fac57ed6ddc5e3e001b93eea807e3
kajasawko <36663999+kajasawko@users.noreply.github.com>
2018-04-09 18:23:23 +0100

    Add files via upload

---
+++ Celcius.java
@@ -0,0 +1,14 @@
+/**
+Name: Convert
+Description: Converts Celcius to Fahrenheit
+Created by: Kaja Sawko
+Created on: 04/04/18
+**/
+
+public class Celcius{
+	public static void main(String[] args){
+
+	Convert.main(null);
+	}
+}
+
